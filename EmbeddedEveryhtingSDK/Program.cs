9ad6c6fa483f9359dc2c58b3702ae7fd61720234
lagrahhn <3220135059@qq.com>
2024-07-06 08:59:21 +0800

    添加集成everything服务项目，可以将检索文件等功能集成到其它应用中。

---
+++ EmbeddedEveryhtingSDK/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EmbeddedEveryhtingSDK;
+
+class Program
+{
+    /// <summary>
+    /// 此 SDK 是一个基础的 IPC 包装器。
+    /// 需要Everything正在运行。
+    /// ANSI/Unicode 支持。
+    /// 安全线程。
+    /// 支持阻挡和非阻挡模式。
+    /// x86 和 x64 支持。
+    /// </summary>
+    const int MAX_RESULTS = 20;
+
+    static void Main(string[] args)
+    {
+        var qry = "everything";
+        var results = Everything.Search(qry);
+
+        var resultCount = 0;
+        foreach (var result in results)
+        {
+            resultCount++;
+            if (resultCount > MAX_RESULTS)
+                break;
+
+            Console.WriteLine($"文件名称{result.Filename}");
+            Console.WriteLine($"文件大小{result.Size}");
+            Console.WriteLine($"文件路径{result.Path}");
+            Console.WriteLine($"修改时间{result.DateModified}");
+        }
+    }
+}
